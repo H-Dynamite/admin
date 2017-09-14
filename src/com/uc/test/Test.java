9d876b2a2561fc5f53cf68408addb2a26b7e0c76
Administrator <248786960@qq.com>
2017-09-14 10:48:19 +0800

    测试

---
+++ src/com/uc/test/Test.java
@@ -0,0 +1,21 @@
+package com.uc.test;
+
+import alex.zhrenjie04.wordfilter2.WordFilterUtil;
+import alex.zhrenjie04.wordfilter2.result.FilteredResult;
+
+public class Test {
+	public static void main(String[] args) { 
+		String str="sahdwiuahlidwÉµ±ÆÈÕÄáÂê²ÙÄãÂè";
+		FilteredResult fr = WordFilterUtil.filterHtml(str,'*');
+		fr.setLevel(1);
+	
+		String str1=fr.getBadWords();
+		String str2=fr.getOriginalContent();
+		int str3=fr.getLevel();
+		String str4=fr.getFilteredContent();
+		System.out.println("getBadWords()"+str1);
+		System.out.println("getOriginalContent"+str2);
+		System.out.println("fr.getLevel()"+str3);
+		System.out.println("getFilteredContent()"+str4);
+    }  
+}
